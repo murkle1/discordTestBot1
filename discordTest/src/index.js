cd76facc316b738f80277a107aa7a0a6abbe8c63
Mark <mdevera_@live.com>
2022-09-01 19:38:39 -0400

    initial commit

---
+++ discordTest/src/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config(); 
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+    ],
+});
+
+
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in`) //string interpolation
+});
+
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return;
+    console.log(`[${message.author.tag}]: ${message.content}`)
+    if (message.content === 'hi'){
+        message.reply('h-hi');
+    }
+    
+}); 
+
+client.login(process.env.DISCORDJS_BOT_TOKEN); // start bot with npm run dev (refers to the script you wrote)
+
